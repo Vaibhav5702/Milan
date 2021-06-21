5d8e42032d32523034698e0c5f3e67497935c806
shrishti <shrishti161@gmail.com>
2021-06-21 17:04:13 +0530

    login sign up

--- app/src/main/java/com/example/milan/LoginRegitser/LoginActivity.java
+++ app/src/main/java/com/example/milan/LoginRegitser/LoginActivity.java
@@ -1,27 +1,49 @@
 package com.example.milan.LoginRegitser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.milan.ChooseActivity;
 import com.example.milan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText mLoginEmailField;
+    private EditText mLoginPasswordField;
+    private ImageButton mLoginBtn;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgress;
     TextView newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         newUser=findViewById(R.id.newUser);
+        mLoginEmailField = (EditText) findViewById( R.id.etEmail );
+        mLoginPasswordField = (EditText) findViewById( R.id.etPassword);
+        mLoginBtn = (ImageButton) findViewById( R.id.btnGo);
+        mAuth = FirebaseAuth.getInstance();
         String user=newUser.getText().toString();
+        mProgress = new ProgressDialog( this );
         Spannable spannable=new SpannableString(user);
         spannable.setSpan(new ForegroundColorSpan(Color.rgb(63,93,201)),10,user.length(),0);
         spannable.setSpan(new UnderlineSpan(),10,user.length(),0);
@@ -31,5 +53,38 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
             startActivity(intent);
         });
+        mLoginBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mLoginEmailField.getText().toString().trim();
+                String password = mLoginPasswordField.getText().toString().trim();
+                if (!TextUtils.isEmpty( email ) || !TextUtils.isEmpty( password )) {
+                    mProgress.setMessage( "logging in" );
+                    mProgress.setCanceledOnTouchOutside( true );
+                    mProgress.show();
+                    loginUser( email, password );
+                }
+            }
+        } );
     }
-}
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword( email, password ).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    mProgress.dismiss();
+                    Intent mainIntent = new Intent( LoginActivity.this, ChooseActivity.class );
+                    mainIntent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+                    startActivity( mainIntent );
+                    finish();
+                } else {
+                    mProgress.hide();
+                    Toast.makeText( LoginActivity.this, "cannot sign in user", Toast.LENGTH_LONG );
+
+                }
+            }
+        } );
+    }
+
+    }
--- app/src/main/java/com/example/milan/LoginRegitser/SignUpActivity.java
+++ app/src/main/java/com/example/milan/LoginRegitser/SignUpActivity.java
@@ -1,26 +1,53 @@
 package com.example.milan.LoginRegitser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.milan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private EditText mNameField;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private ImageButton mRegisterBtn;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgress;
     TextView alreadyUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         alreadyUser=findViewById(R.id.alreadyUser);
+        mProgress=new ProgressDialog( this );
+        mNameField=(EditText) findViewById(R.id.etName);
+        mEmailField=(EditText) findViewById(R.id.etEmail);
+        mPasswordField=(EditText) findViewById(R.id.etPassword);
+        mRegisterBtn=(ImageButton) findViewById(R.id.btnGo);
         String user=alreadyUser.getText().toString();
         Spannable spannable=new SpannableString(user);
         spannable.setSpan(new ForegroundColorSpan(Color.rgb(63,93,201)),16,user.length(),0);
@@ -31,5 +58,42 @@ public class SignUpActivity extends AppCompatActivity {
             Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
             startActivity(intent);
         });
+        mRegisterBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=mNameField.getText().toString().trim();
+                String email=mEmailField.getText().toString().trim();
+                String password=mPasswordField.getText().toString().trim();
+                if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(email)&&!TextUtils.isEmpty(password)){
+                    mProgress.setMessage( "registering user" );
+                    mProgress.setCanceledOnTouchOutside( true );
+                    mProgress.show();
+                    startRegister(name,email,password);}
+            }
+        } );
     }
-}
+
+    private void startRegister(String name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword( email,password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {  mProgress.dismiss();
+
+                    Intent mainIntent =new Intent(SignUpActivity.this,InterestActivity.class);
+                    mainIntent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK );
+                    startActivity( mainIntent );
+
+
+                }
+                else
+                {   mProgress.hide();
+                    String message=task.getException().toString();
+                    Toast.makeText(SignUpActivity.this,message,Toast.LENGTH_LONG).show();
+
+                }
+            }
+        } );
+
+    }
+    }
